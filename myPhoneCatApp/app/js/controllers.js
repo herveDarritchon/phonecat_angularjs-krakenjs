4a91fb947780300fc5e1b864a50e8e913d2440d8
herve darritchon <herve.darritchon@orange.com>
2014-03-21 10:07:56 +0100

    First commit : Externalisation des fichiers statiques (AngularJS) en dehors de NodeJS.

---
+++ myPhoneCatApp/app/js/controllers.js
@@ -0,0 +1,16 @@
+
+var phonecatApp=angular.module('phonecatApp', []);
+/*.config(function($interpolateProvider){
+        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+    }
+);*/
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+	console.log("This is it !");
+	$scope.paragraphTitle="This is my mobile list";
+	$scope.name="World !";
+	$scope.phones = [
+	{'name':'Nexus S','snippet':'Fast just got faster with Nexus S.'},
+	{'name':'Motorola XOOMTM with Wi- Fi','snippet':'The Next, Next Generation tablet.'},
+	{'name':'MOTOROLA XOOMTM','snippet':'The Next, Next Generation tablet.'}
+	];
+});
